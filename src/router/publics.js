d83189fdebcb706d325b0eea9e009307929444f7
17664125975@163.com <duanchaofann@163.com>
2021-07-08 11:11:12 +0800

    增加左侧tab页面

--- src/router/publics.js
+++ src/router/publics.js
@@ -19,37 +19,65 @@ let pubics=[
         name: 'publicsHome',
         component: ()=>import("@/views/publics/Home"),//后台
         children:[
-            {
-                path: '/Home/index',
-                name: 'publicsindex',
-                component: ()=>import("@/views/publics/Home/index"),
-                meta:{breadnav:[{path:'',name:'HOME1'}],name:'HOME1'}//营业数据
-            },
+            // {
+            //     path: '/Home/index',
+            //     name: 'publicsindex',
+            //     component: ()=>import("@/views/publics/Home/index"),
+            //     meta:{breadnav:[{path:'',name:'HOME1'}],name:'HOME1'}//营业数据
+            // },
             {
                 path: '/Home/business',
                 name: 'publicsbusiness',
                 component: ()=>import("@/views/publics/Home/business"),
-                meta:{breadnav:[{path:'',name:'投诉压降'}],name:'投诉压降'}//机构数据
-                // children:[
-                //     {
-                //         path: '/Home/plan',
-                //         name: '/Home/business/plan',
-                //         component: ()=>import("@/views/publics/Home/plan"),
-                //         meta:{breadnav:[{path:'',name:'投诉压降计划'}],name:'投诉压降计划'},//机构数据
-                //     }
-                // ]
+                meta:{breadnav:[{path:'/Home/business',name:'投诉压降'}],name:'投诉压降'}//投诉压降
             },
             {
                 path: '/Home/plan',
-                name: '/Home/business/plan',
+                name: 'businessplan',
                 component: ()=>import("@/views/publics/Home/plan"),
-                meta:{breadnav:[{path:'',name:'投诉压降计划'}],name:'投诉压降计划'}//机构数据
+                meta:{breadnav:[{path:'/Home/plan',name:'投诉压降计划'}],name:'投诉压降计划'}//投诉压降计划
             },
             {
-                path: '/Home/cliniclist',
-                name: 'publicscliniclist',
-                component: ()=>import("@/views/publics/Home/cliniclist"),
-                meta:{breadnav:[{path:'/Home/index',name:'HOME1'},{path:'',name:'HOME2'}],name:'HOME2'}//机构
+                path: '/Home/losspressure',
+                name: 'losspressure',
+                component: ()=>import("@/views/publics/losspressure/losspressure"),
+                meta:{breadnav:[{path:'/Home/losspressure',name:'压降目标'}],name:'压降目标'}//压降目标
+            },
+            {
+                path: '/Home/serch',
+                name: 'serch',
+                component: ()=>import("@/views/publics/search/serch"),
+                meta:{breadnav:[{path:'/Home/serch',name:'查询'}],name:'查询'}//机构
+            },
+            {
+                path: '/Home/regulatorySubmitted',
+                name: 'regulatorySubmitted',
+                component: ()=>import("@/views/publics/regulatorySubmitted/regulatorySubmitted"),
+                meta:{breadnav:[{path:'/Home/regulatorySubmitted',name:'监管报送'}],name:'监管报送'}//监管报送
+            },
+            {
+                path: '/Home/complaintAnalysis',
+                name: 'complaintAnalysis',
+                component: ()=>import("@/views/publics/complaintAnalysis/complaintAnalysis"),
+                meta:{breadnav:[{path:'/Home/complaintAnalysis',name:'投诉分析报告模版'}],name:'投诉分析报告模版'}//投诉分析报告模版
+            },
+            {
+                path: '/Home/fromAudit',
+                name: 'fromAudit',
+                component: ()=>import("@/views/publics/fromAudit/fromAudit"),
+                meta:{breadnav:[{path:'/Home/fromAudit',name:'消保审核'}],name:'消保审核'}//消保审核
+            },
+            {
+                path: '/Home/complaintsWarning',
+                name: 'complaintsWarning',
+                component: ()=>import("@/views/publics/complaintsWarning/complaintsWarning"),
+                meta:{breadnav:[{path:'/Home/complaintsWarning',name:'投诉预警'}],name:'投诉预警'}//投诉预警
+            },
+            {
+                path: '/Home/complaintCockpit',
+                name: 'complaintCockpit',
+                component: ()=>import("@/views/publics/complaintCockpit/complaintCockpit"),
+                meta:{breadnav:[{path:'/Home/complaintCockpit',name:'投诉管理驾驶舱'}],name:'投诉管理驾驶舱'}//投诉管理驾驶舱
             },
        ]
     }
