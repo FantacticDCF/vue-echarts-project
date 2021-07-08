271fa408a5e2525f08fae9833b2e02b38d5666c5
asweetonion <l1995916@qq.com>
2021-07-08 17:28:17 +0800

    首页-大数据组件提取；驾驶舱页面

---
+++ src/views/publics/bigData/bigDataIndex.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <commonTitle></commonTitle>
+    <bigData></bigData>
+  </div>
+</template>
+<script>
+  import commonTitle from "../components/commonTitle.vue";
+  import bigData from "./bigData";
+
+  export default {
+    data() {
+      return {
+      };
+    },
+    components:{
+        commonTitle,
+        bigData
+    },
+    mounted() {
+
+    },
+
+  };
+</script>
